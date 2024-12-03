99095de2b0a4020503f71287cd971f85c9881375
Anthony <64734670+AntMCE@users.noreply.github.com>
2024-12-04 00:18:38 +0100

    Update Login.messages.ts

--- ui/apps/everest/src/pages/login/Login.messages.ts
+++ ui/apps/everest/src/pages/login/Login.messages.ts
@@ -1,8 +1,7 @@
 export const Messages = {
   welcome:
-    'Welcome to Axians, an open source cloud-native database platform!',
+    'Welcome to MyDatanaute, an open source cloud-native database platform powered by Percona !',
   intro: `Percona Everest accelerates code deployment and scaling, and reduces database administration overhead.
-    With the help of Percona Everest, you can regain control over data access, database configuration, and cloud database costs. Provision your first database cluster with Percona Everest today!`,
   login: 'Log in',
   insertCredentials: 'Enter your username and password to log in.',
   ok: 'OK',
