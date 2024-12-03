977bdc0b3550dcf5086d04808dc2ad0e29d7e417
Anthony <64734670+AntMCE@users.noreply.github.com>
2024-12-03 17:50:25 +0100

    Update Login.messages.ts

--- ui/apps/everest/src/pages/login/Login.messages.ts
+++ ui/apps/everest/src/pages/login/Login.messages.ts
@@ -1,6 +1,6 @@
 export const Messages = {
   welcome:
-    'Welcome to Percona Everest, an open source cloud-native database platform!',
+    'Welcome to Axians, an open source cloud-native database platform!',
   intro: `Percona Everest accelerates code deployment and scaling, and reduces database administration overhead.
     With the help of Percona Everest, you can regain control over data access, database configuration, and cloud database costs. Provision your first database cluster with Percona Everest today!`,
   login: 'Log in',
